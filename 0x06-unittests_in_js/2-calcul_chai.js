a0b46c822a9af7ac1e956983b719507ed01cba5d
lannistar1 <51417762+lannistar1@users.noreply.github.com>
2023-09-21 14:50:50 -0500

    Update 2-calcul_chai.js

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,19 +1,14 @@
 const calculateNumber = (type, a, b) => {
-  const aRound = Math.round(a);
-  const bRound = Math.round(b);
-
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
   if (type === 'SUBTRACT') {
-    return aRound - bRound;
+    return Math.round(a) - Math.round(b);
   }
-
   if (type === 'DIVIDE') {
-    if (bRound === 0) {
-      return 'Error';
-    }
-    return aRound / bRound;
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
   }
-
-  return aRound + bRound;
+  return 0;
 };
 
 module.exports = calculateNumber;
