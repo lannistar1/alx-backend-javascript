50dcaf7f0dda271eb2a74f1721269c47c537201c
lannistar1 <51417762+lannistar1@users.noreply.github.com>
2023-09-21 14:59:55 -0500

    Update 6-payment_token.js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,8 +1,7 @@
-const getPaymentTokenFromAPI = (success) => {
-  if (success === true) {
-    return Promise.resolve({ data: 'Successful response from the API' });
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
   }
-  return null;
-};
+});
 
 module.exports = getPaymentTokenFromAPI;
