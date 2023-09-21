bb438d6253ebc2418dec6d6295926ffd396a9290
lannistar1 <51417762+lannistar1@users.noreply.github.com>
2023-09-21 14:58:46 -0500

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,30 +1,29 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-
 const sendPaymentRequestToApi = require('./5-payment');
-const Utils = require('./utils');
 
-describe('Hooks', function () {
-  let spyConsole;
-  beforeEach(function () {
-    spyConsole = sinon.spy(console, 'log');
+describe('sendPaymentRequestToApi', () => {
+  let bigBrother;
+
+  beforeEach(() => {
+    if (!bigBrother) {
+      bigBrother = sinon.spy(console);
+    }
   });
 
-  afterEach(function () {
-    spyConsole.restore();
+  afterEach(() => {
+    bigBrother.log.resetHistory();
   });
 
-  it('logs correctly with 100, 20', () => {
+  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
     sendPaymentRequestToApi(100, 20);
-
-    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
-    expect(spyConsole.calledOnce).to.be.true;
+    expect(bigBrother.log.calledWith('The total is: 120')).to.be.true;
+    expect(bigBrother.log.calledOnce).to.be.true;
   });
 
-  it('logs correctly with 10, 10', () => {
+  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
     sendPaymentRequestToApi(10, 10);
-
-    expect(spyConsole.calledOnceWithExactly('The total is: 20')).to.be.true;
-    expect(spyConsole.calledOnce).to.be.true;
+    expect(bigBrother.log.calledWith('The total is: 20')).to.be.true;
+    expect(bigBrother.log.calledOnce).to.be.true;
   });
 });
